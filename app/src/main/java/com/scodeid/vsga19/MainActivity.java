2d35d8bcfbf4541bfb95011cf84bc2678d14bd63
yogithesymbian <yogirenbox33@gmail.com>
2019-10-12 15:07:52 +0800

    Initial commit

---
+++ app/src/main/java/com/scodeid/vsga19/MainActivity.java
@@ -0,0 +1,28 @@
+package com.scodeid.vsga19;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // declares variable ke view layout
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        final Button btn = findViewById(R.id.btn_move_intent);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // move activity
+                Intent intent = new Intent(MainActivity.this, PuzzleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
