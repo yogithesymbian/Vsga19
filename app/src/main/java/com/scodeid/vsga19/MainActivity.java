ba2f4940234a000ff4a0a551eff9852bfbf63bd8
yogithesymbian <yogirenbox33@gmail.com>
2019-10-12 15:36:01 +0800

    added btn layout puzle

--- app/src/main/java/com/scodeid/vsga19/MainActivity.java
+++ app/src/main/java/com/scodeid/vsga19/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // actionBar
         getSupportActionBar().hide();
 
         final Button btn = findViewById(R.id.btn_move_intent);
