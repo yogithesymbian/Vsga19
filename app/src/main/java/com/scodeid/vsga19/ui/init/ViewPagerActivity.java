77f0c3a7148d9683d87de73ae78c7a180e76f77e
yogithesymbian <yogirenbox33@gmail.com>
2019-10-19 07:28:50 +0800

    view-pager-almost-done

--- app/src/main/java/com/scodeid/vsga19/ui/init/ViewPagerActivity.java
+++ app/src/main/java/com/scodeid/vsga19/ui/init/ViewPagerActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import com.scodeid.vsga19.R;
@@ -12,14 +13,24 @@ public class ViewPagerActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private LinearLayout linearLayout;
+    private SliderAdapter sliderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager);
+        // fullscreen
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
 
         viewPager = findViewById(R.id.vp_slider);
         linearLayout = findViewById(R.id.linear_dot);
+
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
 
     }
 }
