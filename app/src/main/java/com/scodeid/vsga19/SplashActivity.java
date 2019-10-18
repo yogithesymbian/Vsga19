6e6afd88752c816b2203d3b54d7efbe70122ad20
yogithesymbian <yogirenbox33@gmail.com>
2019-10-19 06:00:29 +0800

    added-splash-screen-and-repair-constraint-puzzle

---
+++ app/src/main/java/com/scodeid/vsga19/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.scodeid.vsga19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int delayMiles = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                this.finish();
+            }
+
+            private void finish() {
+
+            }
+        }, delayMiles);
+    }
+}
