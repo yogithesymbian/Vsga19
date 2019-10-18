011c680a5297f52c5566ce3c287b8503579c91e8
yogithesymbian <yogirenbox33@gmail.com>
2019-10-19 06:23:10 +0800

    init-before-added-view-pager-1

--- app/src/main/java/com/scodeid/vsga19/ui/home/MainActivity.java
+++ app/src/main/java/com/scodeid/vsga19/ui/home/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // callBack onBack
     @Override
     public void onBackPressed() {
         super.onBackPressed();
--- app/src/main/java/com/scodeid/vsga19/ui/init/SplashActivity.java
+++ app/src/main/java/com/scodeid/vsga19/ui/init/SplashActivity.java
@@ -24,21 +24,25 @@ public class SplashActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Objects.requireNonNull(getSupportActionBar()).hide();
         }
+        //setFLag this act FULLSCREEN
         this.getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
 
+        // initialize delay
         int delayMiles = 3000;
+        // create Thread Handler with runnable object
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                // intent activity | move activity
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 this.finish();
             }
-
+            // anonymous function
             private void finish() {
-
+                //TODO
             }
         }, delayMiles);
     }
